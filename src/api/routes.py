66506f7dca24423ea88a4b7e6f609ce51655d822
MBello21 <magarbello@gmail.com>
2026-02-25 20:38:35 +0000

    Comprobaciones sin enlace del correo

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,12 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
+from sqlalchemy import select
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import os
+import resend
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +23,26 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/register_user', methods=['POST'])
+def register_user():
+    data = request.get_json()
+
+    if not data.get("email") or not data.get("role"):
+        return jsonify({'error': 'All fields are required'}), 409
+
+    user_exist = db.session.execute(
+        select(User).where(data.get("email") == User.email))
+
+    if user_exist:
+        return jsonify({'error': 'Error processing registration. Please verify the information.'}), 409
+
+    new_user = User(
+        firstname=data.get("firstname"),
+        lastname=data.get("lastname"),
+        rol=data.get("rol"),
+        email=data.get("email"),
+    )
+    db.session.add(new_user)
+    db.session.commit()
