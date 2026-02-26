33ee5f5084b600bf9a3f523ef24d42a7581efba6
MBello21 <magarbello@gmail.com>
2026-02-26 22:00:54 +0000

    Envio de correo de activación

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,6 +6,7 @@ from sqlalchemy import select
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import get_jwt, jwt_required, create_access_token
 import os
 import resend
 
@@ -25,15 +26,15 @@ def handle_hello():
     return jsonify(response_body), 200
 
 
-@api.route('/register_user', methods=['POST'])
+@api.route('/register/user', methods=['POST'])
 def register_user():
     data = request.get_json()
 
-    if not data.get("email") or not data.get("role"):
+    if not data.get("email") or not data.get("rol"):
         return jsonify({'error': 'All fields are required'}), 409
 
     user_exist = db.session.execute(
-        select(User).where(data.get("email") == User.email))
+        select(User).where(data.get("email") == User.email)).scalar_one_or_none()
 
     if user_exist:
         return jsonify({'error': 'Error processing registration. Please verify the information.'}), 409
@@ -43,6 +44,23 @@ def register_user():
         lastname=data.get("lastname"),
         rol=data.get("rol"),
         email=data.get("email"),
+        is_active=False
     )
     db.session.add(new_user)
     db.session.commit()
+
+    additional_claims = {"type": "email_validation"}
+    validation_token = create_access_token(
+        identity=str(new_user.id), additional_claims=additional_claims)
+    resend.api_key = os.environ["RESEND_API_KEY"]
+
+    params: resend.Emails.SendParams = {
+        "from": "Acme <onboarding@resend.dev>",
+        "to": [new_user.email],
+        "subject": "hello world",
+        "html": f"<strong>{validation_token}</strong>",
+    }
+    resend.Emails.send(params)
+
+    return jsonify({'token': validation_token,
+                    'msg': 'Email send successfully'}), 201
