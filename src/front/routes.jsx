e6cbf5eee65f629fd9feea6cdf1a4c8fb432b54f
Lgsus23 <jesusbernardops23@gmail.com>
2026-02-27 11:26:47 +0000

    estética del formulario de login front end terminado, sin fetchs al backend

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -10,6 +10,7 @@ import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { Signup } from "./pages/Signup";
+import { Login } from "./pages/Login/Login";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -27,6 +28,7 @@ export const router = createBrowserRouter(
         <Route path= "/signup" element={<Signup/>} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     )
 );
