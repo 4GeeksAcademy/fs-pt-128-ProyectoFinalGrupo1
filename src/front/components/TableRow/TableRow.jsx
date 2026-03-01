359d2e973ef3cfd44eddb279d6d5968db409d8c6
MBello21 <magarbello@gmail.com>
2026-03-01 13:37:03 +0000

    Maquetación del AP

---
+++ src/front/components/TableRow/TableRow.jsx
@@ -0,0 +1,12 @@
+export const TableRow = () => {
+    return (
+        <tr>
+            <th scope="row">3</th>
+            <td>Migue García</td>
+            <td>magarbello@gmail.com</td>
+            <td>Enfermero</td>
+            <td>active</td>
+            <td><button className="btn btn-outline-danger">Eliminar</button></td>
+        </tr>
+    )
+}
--- src/front/pages/RegisterUser/RegisterUser.jsx
+++ src/front/pages/RegisterUser/RegisterUser.jsx
@@ -1,20 +1,29 @@
+import { ModalAddUser } from "../../components/ModalAddUser/ModalAddUser"
+import { TableRow } from "../../components/TableRow/TableRow"
+
 export const RegisterUser = () => {
     return (
-        <main className="container mt-5">
+        <main className="container mt-5 fontst">
+            <ModalAddUser />
             <div className="border rounded p-3">
                 <div className="d-flex justify-content-center">
                     <h2 className="mt-2 mb-2">Administrador de usuarios</h2>
                 </div>
                 <div>
-                    <table className="table">
-                        <thead>
-                            <tr className="table-primary">
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Apellido</th>
-                                <th scope="col">Rol</th>
+                    <table class="table">
+                        <thead className="table-secondary">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nombre y apellidos</th>
                                 <th scope="col">Email</th>
+                                <th scope="col">Rol</th>
+                                <th scope="col">Activado</th>
+                                <th scope="col">Acciones</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            <TableRow />
+                        </tbody>
                     </table>
                 </div>
             </div>
