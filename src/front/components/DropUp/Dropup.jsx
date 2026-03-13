3023073af599101cefd00c36bd83f99760e53aa9
MBello21 <magarbello@gmail.com>
2026-03-13 12:18:21 +0000

    newNavbar

---
+++ src/front/components/DropUp/Dropup.jsx
@@ -0,0 +1,31 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { ButtonGroup } from 'react-bootstrap';
+import './Dropup.css'
+
+
+export const Dropup = () => {
+    return (
+        <>
+
+            <DropdownButton
+                as={ButtonGroup}
+                key="up"
+                id="dropdown-button-drop-up"
+                drop="up"
+                variant="outline-dark"
+                title={<i class="fa-solid fa-angle-up"></i>}
+                className="custom-dropdown"
+            >
+                {/* <Dropdown.Divider /> */}
+                <Dropdown.Item eventKey="1" className='text-danger d-flex align-items-center '>
+                    <i class="fa-solid fa-arrow-right-from-bracket me-1"></i>
+                    Cerrar sesión
+                </Dropdown.Item>
+            </DropdownButton>
+
+
+
+        </>
+    );
+}
