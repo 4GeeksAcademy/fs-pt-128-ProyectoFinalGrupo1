d3d1f47312019a9b5e512299c6a90388e2539a90
Alberto Madrid <albertomadrid93@gmail.com>
2026-03-22 22:23:17 +0100

    Import signup function from BACKENDservices

--- src/front/pages/Signup.jsx
+++ src/front/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer"
+import { signup } from "../APIServices/BACKENDservices";
 
 export const Signup = () => {
 
