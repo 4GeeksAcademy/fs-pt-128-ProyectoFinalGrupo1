a6791af0e59574ac7889f83e951f0bec0563f77e
MBello21 <magarbello@gmail.com>
2026-03-23 00:06:02 +0100

    Update BACKENDservices.js

--- src/front/APIServices/BACKENDservices.js
+++ src/front/APIServices/BACKENDservices.js
@@ -15,24 +15,17 @@ export const login = async (user, navigate) => {
   if (!response.ok) {
     return data;
   }
+  
   localStorage.setItem("token", data.token);
-  const profile = await getProfile();
-  const rol = localStorage.getItem("rol");
-
+  const profile = await getProfile(); 
+  const userRol = profile.rol || data.user.rol; 
   navigate(
-    user.rol === "Administrativo"
-      ? "/admission"
-      : user.rol === "Enfermero"
-        ? "/control-panel/triage"
-        : user.rol === "Médico"
-          ? "/control-panel/consultation"
-          : user.rol === "Admin"
-            ? "/register-user"
-            : user.rol === "Técnico"
-              ? "/medical-test"
-              : "/",
+      userRol === "Administrativo" ? "/admission" : 
+      userRol === "Enfermero" ? "/control-panel/triage" : 
+      userRol === "Médico" ? "/control-panel/consultation" : 
+      userRol === "Admin" ? "/register-user" : 
+      userRol === "Técnico" ? "/medical-test" : "/"
   );
-  return data;
 };
 
 // region: activateCount
